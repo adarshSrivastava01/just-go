cbbd6e0c6272395f1ad197df41b28f908126e0a9
Adarsh Srivastava <adarshsrivastava.tech@gmail.com>
2022-03-09 22:32:03 +0530

    Format Printting and variables

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	
+	fmt.Println("Welcome to", conferenceName, "booking application", conferenceTickets)
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+
 }
