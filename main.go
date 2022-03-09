fbda42f17f431994087f1e13e4ed5ba0e9459535
Adarsh Srivastava <adarshsrivastava.tech@gmail.com>
2022-03-09 22:57:12 +0530

    Go Arrays

--- main.go
+++ main.go
@@ -6,17 +6,40 @@ func main() {
 	// var conferenceName string = "Go Conference"
 	conferenceName := "Go Conference" // can't be done in case of constants
 	const conferenceTickets uint = 50
+	var remainingTickets  uint = 50
 	
 	fmt.Println("Welcome to", conferenceName, "booking application", conferenceTickets)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 
-	var userName string
-	var userTickets int
+	// var bookings = [50]string{} // length of array -> 50, type of elements -> string, {} -> Initialised Values
+	// or could be replaced by
+	
+	var bookings [50]string
 
-	fmt.Scan(&userName) // fmt.Println(&name) -> returns the memory address
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 
-	fmt.Printf("User %v has %v Tickets", userName, userTickets)
+	fmt.Println("Enter the first Name: ")
+	fmt.Scan(&firstName) // fmt.Println(&name) -> returns the memory address
+	fmt.Println("Enter the last Name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter the Email: ")
+	fmt.Scan(&email)
+	fmt.Println("Enter the No of Tickets: ")
+	fmt.Scan(&userTickets)
 
-	fmt.Printf("Type of variable can be achieved using %T\n", conferenceTickets)
+	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The Whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array Type: %T\n", bookings)
+	fmt.Printf("Array Length: %v\n", len(bookings))
+
+	// fmt.Printf("User %v has %v Tickets", firstName, userTickets)
+
+	// fmt.Printf("Type of variable can be achieved using %T\n", conferenceTickets)
 }
