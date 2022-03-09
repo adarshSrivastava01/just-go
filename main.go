bfec39571acbb04574189e57584d84db8327fd7e
Adarsh Srivastava <adarshsrivastava.tech@gmail.com>
2022-03-09 22:39:23 +0530

    Data types and typeof

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
+	var conferenceName string = "Go Conference"
+	const conferenceTickets uint = 50
 	
 	fmt.Println("Welcome to", conferenceName, "booking application", conferenceTickets)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 
+	var userName string
+	var userTickets int
+
+	userName = "Tom"
+	userTickets = 2
+
+	fmt.Printf("User %v has %v Tickets", userName, userTickets)
+
+	fmt.Printf("Type of variable can be achieved using %T\n", conferenceTickets)
 }
