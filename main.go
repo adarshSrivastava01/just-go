1c42be7f37ca61ba36da86b54de98d1ccca7e8ae
Adarsh Srivastava <adarshsrivastava.tech@gmail.com>
2022-03-09 23:12:36 +0530

    Go ForEach + Conditionals

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	// var conferenceName string = "Go Conference"
@@ -19,30 +22,50 @@ func main() {
 	// to create slice i.e. list we just remove the size from declaration -> var bookings []string
 	// to add element use sliceName = append(sliceName, value)
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	// for { // Statements // } -> Infinite loop
 
-	fmt.Println("Enter the first Name: ")
-	fmt.Scan(&firstName) // fmt.Println(&name) -> returns the memory address
-	fmt.Println("Enter the last Name: ")
-	fmt.Scan(&lastName)
-	fmt.Println("Enter the Email: ")
-	fmt.Scan(&email)
-	fmt.Println("Enter the No of Tickets: ")
-	fmt.Scan(&userTickets)
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+	
+		fmt.Println("Enter the first Name: ")
+		fmt.Scan(&firstName) // fmt.Println(&name) -> returns the memory address
+		fmt.Println("Enter the last Name: ")
+		fmt.Scan(&lastName)
+		fmt.Println("Enter the Email: ")
+		fmt.Scan(&email)
+		fmt.Println("Enter the No of Tickets: ")
+		fmt.Scan(&userTickets)
+	
+		remainingTickets = remainingTickets - userTickets
+		// bookings[0] = firstName + " " + lastName
+		bookings = append(bookings, firstName + " " + lastName)
+	
+		fmt.Printf("The Whole slice: %v\n", bookings)
+		fmt.Printf("The first value: %v\n", bookings[0])
+		fmt.Printf("Slice Type: %T\n", bookings)
+		fmt.Printf("Slice Length: %v\n", len(bookings))
+	
+		// fmt.Printf("User %v has %v Tickets", firstName, userTickets)
+	
+		// fmt.Printf("Type of variable can be achieved using %T\n", conferenceTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	// bookings[0] = firstName + " " + lastName
-	bookings = append(bookings, firstName + " " + lastName)
+		firstNames := []string{}
+		// forEach loop in Go
+		for _, booking := range bookings { // _ for unused variables // range -> same as enumerate in python can be used on arrays, slices, maps and more
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The First names of bookings are: %v\n", firstNames)
 
-	fmt.Printf("The Whole slice: %v\n", bookings)
-	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("Slice Type: %T\n", bookings)
-	fmt.Printf("Slice Length: %v\n", len(bookings))
+		noTicketsRemaining := remainingTickets == 0
+		if noTicketsRemaining {
+			// end the program
+			fmt.Println("Our conference is booked out")
+			break
+		}
+	}
 
-	// fmt.Printf("User %v has %v Tickets", firstName, userTickets)
-
-	// fmt.Printf("Type of variable can be achieved using %T\n", conferenceTickets)
 }
